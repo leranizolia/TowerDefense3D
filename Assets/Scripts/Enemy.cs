25a4e9c04877de540c64906834323362a7f00c16
Валерия Низоля <leranizolya@yandex.ru>
2021-08-29 16:11:57 +0300

    hot reload done

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -77,6 +77,15 @@ public class Enemy : GameBehavior
 
     public override bool GameUpdate()
     {
+#if UNITY_EDITOR
+		if (!animator.IsValid) {
+			animator.RestoreAfterHotReload(
+				model.GetChild(0).GetComponent<Animator>(),
+				animationConfig,
+				animationConfig.MoveAnimationSpeed * speed / Scale
+			);
+		}
+#endif
         animator.GameUpdate();
 
         if (animator.CurrentClip == EnemyAnimator.Clip.Intro)
